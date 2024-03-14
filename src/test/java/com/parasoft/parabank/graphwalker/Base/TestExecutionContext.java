dbdef4074cf1f3ea0b9550fca34662c03a83dfe6
MaximGonnissen <33728489+MaximGonnissen@users.noreply.github.com>
2024-03-14 03:34:38 +0100

    Create TestExecutionContext extension of ExecutionContext
    Used to add a logger and some debugging prints

---
+++ src/test/java/com/parasoft/parabank/graphwalker/Base/TestExecutionContext.java
@@ -0,0 +1,19 @@
+package com.parasoft.parabank.graphwalker.Base;
+
+import org.graphwalker.core.machine.ExecutionContext;
+import org.graphwalker.java.annotation.BeforeElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestExecutionContext extends ExecutionContext {
+    protected static Logger logger = null;
+
+    public TestExecutionContext() {
+        logger = LoggerFactory.getLogger(this.getClass());
+    }
+
+    @BeforeElement
+    public void setup() {
+        logger.info("Entering: " + getCurrentElement().getName());
+    }
+}
