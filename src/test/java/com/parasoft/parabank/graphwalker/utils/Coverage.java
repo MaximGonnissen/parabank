5b00b37d0fc5a2f7c36b64b4307cd193855a7b26
MaximGonnissen <33728489+MaximGonnissen@users.noreply.github.com>
2024-03-14 03:33:49 +0100

    Create Coverage static strings

---
+++ src/test/java/com/parasoft/parabank/graphwalker/utils/Coverage.java
@@ -0,0 +1,11 @@
+package com.parasoft.parabank.graphwalker.utils;
+
+/**
+ * Holds coverage values for the graph walker tests
+ */
+public class Coverage {
+    public static final String RandomEdgeCoverage100 = "random(edge_coverage(100))";
+    public static final String QuickRandomEdgeCoverage100 = "quick_random(edge_coverage(100))";
+    public static final String WeightedRandomEdgeCoverage100 = "weighted_random(edge_coverage(100))";
+    public static final String AStarReachedVertex = "a_star(reached_vertex())"; // TODO: Add reached vertex
+}
