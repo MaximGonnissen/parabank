7fe8471b06cd6118a77395a528cf30a3b71dec30
MaximGonnissen <33728489+MaximGonnissen@users.noreply.github.com>
2024-05-06 14:30:06 +0200

    Fix logger being overwritten

--- src/test/java/com/parasoft/parabank/graphwalker/Base/TestExecutionContext.java
+++ src/test/java/com/parasoft/parabank/graphwalker/Base/TestExecutionContext.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TestExecutionContext extends ExecutionContext {
-    protected static Logger logger = null;
+    protected Logger logger = null;
     public static final String MODEL_PATH = "org/graphwalker/parabank_model.json";
 
     public TestExecutionContext() {
