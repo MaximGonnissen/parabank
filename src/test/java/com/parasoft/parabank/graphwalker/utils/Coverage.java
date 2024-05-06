736f3890cbf6556527dd2ce0c1a37c6a275bd078
MaximGonnissen <33728489+MaximGonnissen@users.noreply.github.com>
2024-05-06 20:28:05 +0200

    Change default coverage

--- src/test/java/com/parasoft/parabank/graphwalker/utils/Coverage.java
+++ src/test/java/com/parasoft/parabank/graphwalker/utils/Coverage.java
@@ -9,5 +9,5 @@ public class Coverage {
     public static final String WeightedRandomEdgeCoverage100 = "weighted_random(edge_coverage(100))";
     public static final String ShortestAllPathsEdgeCoverage100 = "shortest_all_paths(edge_coverage(100))";
     public static final String DirectedChinesePostmanCoverage = "directed_chinese_postman(edge_coverage(100))";
-    public static final String Default = RandomEdgeCoverage100;
+    public static final String Default = DirectedChinesePostmanCoverage;
 }
