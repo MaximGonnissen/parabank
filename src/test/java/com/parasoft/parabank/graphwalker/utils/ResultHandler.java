c08be2fdfdbe28b2d7b2739b322c0d1deb1af8de
MaximGonnissen <33728489+MaximGonnissen@users.noreply.github.com>
2024-05-28 01:45:07 +0200

    Add verbose param

--- src/test/java/com/parasoft/parabank/graphwalker/utils/ResultHandler.java
+++ src/test/java/com/parasoft/parabank/graphwalker/utils/ResultHandler.java
@@ -3,8 +3,14 @@ package com.parasoft.parabank.graphwalker.utils;
 import org.graphwalker.java.test.Result;
 
 public class ResultHandler {
+
     public static boolean handleResult(Result result, boolean isolatedTest) {
-        System.out.println("Test finished with result: " + result.getResults().toString());
+        return handleResult(result, isolatedTest, false);
+    }
+
+    public static boolean handleResult(Result result, boolean isolatedTest, boolean verbose) {
+        if (verbose)
+            System.out.println("Test finished with result: " + result.getResults().toString());
 
         if (result.hasErrors()) {
             if (isolatedTest && result.getErrors().size() == 1 && result.getErrors().get(0).contains("Could not find a valid path from element")) {
